7df4ddbf64feafeebeac3ed03e38e04cdf08c20a
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-09 16:43:15 +0500

    Лешино задание

---
+++ Lesha_zadanie/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int length = (int)Math.Log10(number) + 1;
+int i = 0;
+int step = length - 1;
+
+while (i < length)
+{
+    int chisl = (number / (int)Math.Pow(10, step) % 10);
+    Console.Write(chisl + " ");
+    i++;
+    step--;
+}
+
