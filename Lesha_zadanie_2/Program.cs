331abd9c0f895a31f7a57d162ee5b48fe9a81aca
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-11 16:56:13 +0500

    Lesha task 2 start

---
+++ Lesha_zadanie_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = ReadInt("Введите число: "); 
+int[] array = new int[n];
+array[0] = 1;
+
+for  (int i = 0; i < n; i++)
+{
+    Console.Write(array[i]);
+}
