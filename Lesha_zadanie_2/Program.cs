b0c7ec3e27c6e534b2b7b6c1a014f0aac12efd9d
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-12 14:33:24 +0500

    Lesha task 2 fucking done 2

--- Lesha_zadanie_2/Program.cs
+++ Lesha_zadanie_2/Program.cs
@@ -12,6 +12,7 @@ int z = 0;
 int g = 0;
 int indexI = 0;
 int indexJ = 0;
+int a = (int)Math.Log10(n * n) + 2;
 
 for  (indexI = 0; indexI < n; indexI++)
 {
@@ -28,8 +29,7 @@ for  (j = 0; j < n; j++)
     i = g;
     if (mas[i,j] == 0)
         {
-        mas[i,j] = z + 1;
-        z++;  
+        mas[i,j] = ++z;  
         }
 }
 for (i = 0; i < n; i++)
@@ -37,8 +37,7 @@ for (i = 0; i < n; i++)
     j = n - g - 1;
     if (mas[i,j] == 0)
         {
-        mas[i,j] = z + 1; 
-        z++;  
+        mas[i,j] = ++z; 
         }   
 }
 for  (j = n - 1; j >= 0; j--)
@@ -46,8 +45,7 @@ for  (j = n - 1; j >= 0; j--)
     i = n - g - 1;
     if (mas[i,j] == 0)
         {
-        mas[i,j] = z + 1;
-        z++;  
+        mas[i,j] = ++z;  
         }  
 }
 for (i = n - 1; i >= 0; i--)
@@ -55,26 +53,25 @@ for (i = n - 1; i >= 0; i--)
     j = g;
     if (mas[i,j] == 0)
         {
-        mas[i,j] = z + 1;
-        z++;  
+        mas[i,j] = ++z;
         }  
 }
 g++;
 }
-i = 0;
-j = 0;
+
 for  (i = 0; i < n; i++)
 {
     for (j = 0; j < n; j++)
     {
-        if (mas[i,j] < 10)
+        /*if (mas[i,j] < 10)
         {
             Console.Write(mas[i,j] + "  ");
         }
         else
         {
             Console.Write(mas[i,j] + " ");
-        }        
+        } */
+        Console.Write("{0," + a + "}", mas[i, j]);       
     }
     Console.WriteLine();
 }
