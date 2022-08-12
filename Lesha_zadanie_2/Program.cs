3256b934fc52792faf73f99b82c66ca5acde6dfa
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-12 13:59:08 +0500

    Lesha task 2 fucking done

--- Lesha_zadanie_2/Program.cs
+++ Lesha_zadanie_2/Program.cs
@@ -5,20 +5,75 @@
 }
 
 int n = ReadInt("Введите число: "); 
-int[] array = new int[n];
+int[,] mas = new int[n, n];
+int i = 0;
+int j = 0;
+int z = 0;
+int g = 0;
+int indexI = 0;
+int indexJ = 0;
 
-for  (int i = 0; i < n; i++)
+for  (indexI = 0; indexI < n; indexI++)
 {
-    for (int j = 0; j < n; j++)
+    for (indexJ = 0; indexJ < n; indexJ++)
     {
-        array[j] = i * n + j +1;
-        if (array[j] < 10)
+        mas[indexI, indexJ] = 0;      
+    }
+}
+
+while (g < n)
+{
+for  (j = 0; j < n; j++)
+{
+    i = g;
+    if (mas[i,j] == 0)
         {
-            Console.Write(array[j] + "  ");
+        mas[i,j] = z + 1;
+        z++;  
+        }
+}
+for (i = 0; i < n; i++)
+{
+    j = n - g - 1;
+    if (mas[i,j] == 0)
+        {
+        mas[i,j] = z + 1; 
+        z++;  
+        }   
+}
+for  (j = n - 1; j >= 0; j--)
+{
+    i = n - g - 1;
+    if (mas[i,j] == 0)
+        {
+        mas[i,j] = z + 1;
+        z++;  
+        }  
+}
+for (i = n - 1; i >= 0; i--)
+{
+    j = g;
+    if (mas[i,j] == 0)
+        {
+        mas[i,j] = z + 1;
+        z++;  
+        }  
+}
+g++;
+}
+i = 0;
+j = 0;
+for  (i = 0; i < n; i++)
+{
+    for (j = 0; j < n; j++)
+    {
+        if (mas[i,j] < 10)
+        {
+            Console.Write(mas[i,j] + "  ");
         }
         else
         {
-            Console.Write(array[j] + " ");
+            Console.Write(mas[i,j] + " ");
         }        
     }
     Console.WriteLine();
