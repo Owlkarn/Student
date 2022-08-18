401f28a788a4ba3665f06f9b07d59aaef7c59ebf
Vedeneev_Ilya <tegrozayadc@yandex.ru>
2022-08-18 09:05:39 +0500

    Lesha_zadanie 4 done

---
+++ Lesha_zadanie_4/Program.cs
@@ -0,0 +1,30 @@
+﻿void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SortArray (int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[i])
+            {
+                int min = array[j];
+                array[j] = array[i];
+                array[i] = min;
+            }
+        }
+    }
+}
+
+int[] mas = {4, 3, 6, 1, 8, 9, 0, 2};
+
+PrintArray(mas);
+SortArray(mas);
+PrintArray(mas);
